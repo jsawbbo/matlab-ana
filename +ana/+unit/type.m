a62b0ed7a0f434d4930a66949d61fd066b44c4ed
Jürgen "George" Sawinski <juergen.sawinski@mpinb.mpg.de>
2026-01-15 13:48:27 +0100

    units, maybe...

---
+++ +ana/+unit/type.m
@@ -0,0 +1,19 @@
+classdef type
+    %ANA.UNIT.TYPE         Unit types.
+    %
+    %   Detailed explanation goes here
+    %
+
+    properties
+        L = 0
+        M = 0
+        T = 0
+    end
+
+    methods
+        function obj = types()
+            %TYPES Construct an instance of this class
+            %   Detailed explanation goes here
+        end
+    end
+end
